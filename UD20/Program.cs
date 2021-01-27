0f6f1b7a0abbde862e9016ba51d87f8c8dfe637b
SeClaMa <76478324+SeClaMa@users.noreply.github.com>
2021-01-27 11:23:54 +0100

    UD20

---
+++ UD20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UD20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
